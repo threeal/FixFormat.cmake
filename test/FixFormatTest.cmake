2fc4e0bc82957e69dc44a57a74f76465a450d84f
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-01-14 20:23:02 +0700

    test: organize test files to support multiple cases

--- test/FixFormatTest.cmake
+++ test/FixFormatTest.cmake
@@ -1,55 +1,70 @@
-set(
-  SRCS
-  include/fibonacci.hpp
-  include/is_odd.hpp
-  src/fibonacci.cpp
-  src/is_odd.cpp
-)
+# Matches everything if not defined
+if(NOT TEST_MATCHES)
+  set(TEST_MATCHES ".*")
+endif()
 
-message(STATUS "Getting the original source file hashes")
-foreach(SRC ${SRCS})
-  file(MD5 ${CMAKE_CURRENT_LIST_DIR}/sample/${SRC} ${SRC}_HASH)
-endforeach()
+set(TEST_COUNT 0)
 
-message(STATUS "Copying the ugly source files")
-foreach(SRC ${SRCS})
-  file(
-    COPY_FILE
-    ${CMAKE_CURRENT_LIST_DIR}/sample/dirty/${SRC}
-    ${CMAKE_CURRENT_LIST_DIR}/sample/${SRC}
+if("Testing source codes formatting" MATCHES ${TEST_MATCHES})
+  math(EXPR TEST_COUNT "${TEST_COUNT} + 1")
+
+  set(
+    SRCS
+    include/fibonacci.hpp
+    include/is_odd.hpp
+    src/fibonacci.cpp
+    src/is_odd.cpp
   )
-endforeach()
 
-if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/sample/build)
-  message(STATUS "Removing build directory")
-  file(REMOVE_RECURSE ${CMAKE_CURRENT_LIST_DIR}/sample/build)
-endif()
+  message(STATUS "Getting the original source file hashes")
+  foreach(SRC ${SRCS})
+    file(MD5 ${CMAKE_CURRENT_LIST_DIR}/sample/${SRC} ${SRC}_HASH)
+  endforeach()
 
-message(STATUS "Configuring sample project")
-execute_process(
-  COMMAND
-    cmake ${CMAKE_CURRENT_LIST_DIR}/sample
-      -B ${CMAKE_CURRENT_LIST_DIR}/sample/build
-      -D CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
-  RESULT_VARIABLE RES
-)
-if(NOT ${RES} EQUAL 0)
-  message(FATAL_ERROR "Failed to configure sample project")
-endif()
+  message(STATUS "Copying the ugly source files")
+  foreach(SRC ${SRCS})
+    file(
+      COPY_FILE
+      ${CMAKE_CURRENT_LIST_DIR}/sample/dirty/${SRC}
+      ${CMAKE_CURRENT_LIST_DIR}/sample/${SRC}
+    )
+  endforeach()
 
-message(STATUS "Building sample project")
-execute_process(
-  COMMAND cmake --build ${CMAKE_CURRENT_LIST_DIR}/sample/build
-  RESULT_VARIABLE RES
-)
-if(NOT ${RES} EQUAL 0)
-  message(FATAL_ERROR "Failed to build sample project")
-endif()
-
-message(STATUS "Comparing the source file hashes")
-foreach(SRC ${SRCS})
-  file(MD5 ${CMAKE_CURRENT_LIST_DIR}/sample/${SRC} HASH)
-  if(NOT ${HASH} STREQUAL ${${SRC}_HASH})
-    message(FATAL_ERROR "File hash of ${SRC} is different: got ${HASH}, should be ${${SRC}_HASH}")
+  if(EXISTS ${CMAKE_CURRENT_LIST_DIR}/sample/build)
+    message(STATUS "Removing build directory")
+    file(REMOVE_RECURSE ${CMAKE_CURRENT_LIST_DIR}/sample/build)
   endif()
-endforeach()
+
+  message(STATUS "Configuring sample project")
+  execute_process(
+    COMMAND
+      cmake ${CMAKE_CURRENT_LIST_DIR}/sample
+        -B ${CMAKE_CURRENT_LIST_DIR}/sample/build
+        -D CMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}
+    RESULT_VARIABLE RES
+  )
+  if(NOT ${RES} EQUAL 0)
+    message(FATAL_ERROR "Failed to configure sample project")
+  endif()
+
+  message(STATUS "Building sample project")
+  execute_process(
+    COMMAND cmake --build ${CMAKE_CURRENT_LIST_DIR}/sample/build
+    RESULT_VARIABLE RES
+  )
+  if(NOT ${RES} EQUAL 0)
+    message(FATAL_ERROR "Failed to build sample project")
+  endif()
+
+  message(STATUS "Comparing the source file hashes")
+  foreach(SRC ${SRCS})
+    file(MD5 ${CMAKE_CURRENT_LIST_DIR}/sample/${SRC} HASH)
+    if(NOT ${HASH} STREQUAL ${${SRC}_HASH})
+      message(FATAL_ERROR "File hash of ${SRC} is different: got ${HASH}, should be ${${SRC}_HASH}")
+    endif()
+  endforeach()
+endif()
+
+if(TEST_COUNT LESS_EQUAL 0)
+  message(FATAL_ERROR "Nothing to test with: ${TEST_MATCHES}")
+endif()
