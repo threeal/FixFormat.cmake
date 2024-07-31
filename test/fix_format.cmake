76d1513a1bdc72cab8e598eb281f66133a3a8dcb
Alfi Maulana <alfi.maulana.f@gmail.com>
2024-07-31 21:48:56 +0700

    test: utilize `assertion_add_test` to create CMake test

--- test/fix_format.cmake
+++ test/fix_format.cmake
@@ -1,11 +1,3 @@
-cmake_minimum_required(VERSION 3.5)
-
-file(
-  DOWNLOAD https://github.com/threeal/assertion-cmake/releases/download/v1.0.0/Assertion.cmake
-    ${CMAKE_BINARY_DIR}/Assertion.cmake
-  EXPECTED_MD5 1d8ec589d6cc15772581bf77eb3873ff)
-include(${CMAKE_BINARY_DIR}/Assertion.cmake)
-
 function(check_source_codes_format)
   set(OPTIONS USE_GLOBAL_FORMAT USE_FILE_SET_HEADERS FORMAT_TWICE)
   cmake_parse_arguments(PARSE_ARGV 0 ARG "${OPTIONS}" "" "SRCS;FORMAT_TARGETS")
